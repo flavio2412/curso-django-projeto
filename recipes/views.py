73576a1ce6fad1e5cc536f4e058dd13a2b8438fd
Flavio Da Silva <flaviosilva2412@icloud.com>
2023-06-22 16:17:52 -0300

    revovido conta e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request,'recipes/home.html', context={
                   'name': 'Flavio ',})
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
