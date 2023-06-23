9c75431862cb177f6ce57ea5276eabca3331d606
Flavio Da Silva <flaviosilva2412@icloud.com>
2023-06-23 15:48:28 -0300

    atualizaçòes

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'recipes/home.html', context={
+    return render(request,'recipes/pages/home.html', context={
                   'name': 'Flavio ',})
